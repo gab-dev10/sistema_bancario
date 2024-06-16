8ccd7338796e221561137c63eb6c9b04088907c1
Gabriel Cabral Manhães <158057306+gab-dev10@users.noreply.github.com>
2024-06-16 15:23:32 -0300

    Projeto

---
+++ sistema_bancario.py
@@ -0,0 +1,63 @@
+menu = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True: 
+    opcao = input(menu)
+
+    if opcao == "d":
+        print("Depósito...")
+        valor_deposito = float(input("Informe o valor do depósito: "))
+
+        if valor_deposito > 0:
+            saldo += valor_deposito
+            extrato += f"Depósito: R$ {valor_deposito:.2f}\n"
+        else:
+            print("Não é possível depositar valores menores que 1 Real")
+
+    elif opcao == "s":
+        valor_saque = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor_saque > saldo
+        excedeu_limite = valor_saque > limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excedeu o limite.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+
+        elif valor_saque > 0:
+            saldo -= valor_saque
+            extrato += f"Saque: R$ {valor_saque:.2f}\n"
+            numero_saques += 1
+        else:
+            print("Operação falhou! O valor inserido é inválido.")
+
+    elif opcao == "e":
+        print("\n ===== EXTRATO =====")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("======================")
+
+    elif opcao == "q":
+        print("Saindo...")
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
